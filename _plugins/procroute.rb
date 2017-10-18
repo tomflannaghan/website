1d9a9bc21c83003a18baa1fb09534073d3b094d4
Tom Flannaghan <tomflannaghan@gmail.com>
2017-10-18 14:49:25 +0100

    various fixes due to Jenkins upgrade

--- _plugins/procroute.rb
+++ _plugins/procroute.rb
@@ -12,13 +12,13 @@ module Jekyll
       kml_template = File.read("cycling/kml/template.kml")
       ## accumulate input to the python script for graph plotting
       python_input = []
-      
+
       site.pages.each do |page|
         ## Ignore pages outside of the /cycling/routes folder
         if  ( ! ( page.url =~ /^\/cycling\/routes\// ) )
           next
         end
-        
+
         if not page.data['route']
           raise "No route variable found for #{page.name}.\n"
         end
@@ -29,23 +29,23 @@ module Jekyll
         kml_fs = ".#{kml_file}"
         gpx_fs = ".#{gpx_file}"
         alt_fs = ".#{alt_file}"
-        
+
         ## GPX file must exist!
         if not File.exist?(gpx_fs)
           raise "No GPX file found at #{gpx_fs}\n"
         end
         print "Generating #{kml_fs}...\n"
-        
+
         ## initialize metadata that will be gathered
         total_climb = 0
         last_point = nil
         coords_for_kml = ""
         elevation_profile_data = []
         total_distance = 0
-        
+
         ## first, simplify the gpx file. we need a temp file.
-        tmp_fs = Dir::Tmpname.create(['working', '.gpx']) { |f| 
-          raise Errno::EEXIST if File.exist? f 
+        tmp_fs = Dir::Tmpname.create(['working', '.gpx']) { |f|
+          raise Errno::EEXIST if File.exist? f
         }
         error = 0.001  ## accepted error in distance. 0.1% gives good results.
         cmd = "gpsbabel -i gpx -f #{gpx_fs} -x  simplify,error=#{error} -o gpx -F #{tmp_fs}"
@@ -53,7 +53,7 @@ module Jekyll
         if not wasGood
           raise "GPSBabel failed to simplify '#{gpx_fs}'. Command was 'gpsbabel -i gpx -f #{gpx_fs} -x  simplify,error=#{error} -o gpx -F #{tmp_fs}'"
         end
-        
+
         ## process the gpx file and calculate metadata
         gpx = GPX::GPXFile.new(:gpx_file => tmp_fs)
         track = gpx.tracks[0]
@@ -76,22 +76,22 @@ module Jekyll
             last_point = point
           end
         end
-        
+
         ## unlink the temporary file
         File.unlink(tmp_fs)
-        
+
         ## write the KML file.
         f = File.open(kml_fs, 'w')
         f.write(kml_template % {:coords => coords_for_kml})
         f.close()
         push_static_file(site, kml_fs)
-        
+
         ## the alt files are processed in bulk by python --> more efficient.
         python_input << {
-          :profile_data => elevation_profile_data, 
+          :profile_data => elevation_profile_data,
           :alt_file => alt_fs}
         push_static_file(site, alt_fs) ## ok to do before file created.
-        
+
         page.data['kml'] = kml_file
         page.data['gpx'] = gpx_file
         page.data['altimage'] = alt_file
@@ -99,7 +99,7 @@ module Jekyll
         page.data['climb'] = total_climb.round
         page.data['bikeroute'] = "yes" ## this is used to enumerate rides.
       end
-      
+
       ## finally call the python to actually generate images.
       IO.popen(["python", "_plugins/procroute.py"], mode="w+") do |io|
         io.write(JSON.generate(python_input))
--- build_and_publish.sh
+++ build_and_publish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # python poole_comments.py
+jekyll clean
 jekyll build
 rsync -rv _site/* flannaghan:public_html/.
